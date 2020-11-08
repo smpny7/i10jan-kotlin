9b7f993e70a46581f62a5eb6e431e2a494c571ba
smpny7 <symphonymode7@icloud.com>
2020-11-08 13:42:35 +0900

    QRコード 読み込み完成

--- app/src/main/java/net/oucrc/MainActivity.kt
+++ app/src/main/java/net/oucrc/MainActivity.kt
@@ -1,11 +1,33 @@
 package net.oucrc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val integrator = IntentIntegrator(this)
+        integrator.setOrientationLocked(false)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("名札をかざしてください")
+        integrator.setCameraId(1) // Use a specific camera of the device
+
+        integrator.setOrientationLocked(false)
+        integrator.setBeepEnabled(false)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        when {
+            result != null -> Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+            else -> super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
